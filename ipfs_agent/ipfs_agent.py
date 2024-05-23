e13d1c37c81a3d91317a74c81f7f98d4362ec187
endomorphosis <starworks5@gmail.com>
2024-05-22 20:28:52 -0700

    chore: Remove unused code and dependencies in ipfs_agent

--- ipfs_agent/ipfs_agent.py
+++ ipfs_agent/ipfs_agent.py
@@ -1,16 +1,23 @@
 import ipfs_model_manager as model_manager
-
+import config as config
+import orbitdb_kit_lib as orbitdb_kit
 class ipfs_agent:
     def __init__(self, resources=None, meta=None):
+        self.config = config.config()
         self.model_manager = model_manager.model_manager(resources, meta)
         pass
 
-
     def __call__(self, input):
-        return input
+        config = self.config
+        models = self.model_manager.list_ipfs_models()
+        results = {
+            "config": config,
+            "models": models
+        }
+        return results
     
 if __name__ == '__main__':
     meta = {}
     resources = {}
     ipfs_agent = ipfs_agent(resources, meta)
-    print(ipfs_agent("Hello, world!"))
+    print(ipfs_agent())
---
+++ ipfs_agent/orbitdb_kit.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import subprocess as process 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from ipfs_transformers.orbit_kit import orbit_kit
+import websockets as ws 
+from config import config
+
+class orbit_kit:
+    def __init__(self, resources, meta=None):
+        self.resources = resources
+        self.meta = meta
+        self.config = config
+        self.orbitdb_args = {}
+        if self.meta is None:
+            self.meta = {}
+            self.orbitdb_args['ipaddress'] = None
+            self.orbitdb_args['orbitdbAddress'] = None
+            self.orbitdb_args['index'] = None
+            self.orbitdb_args['chunkSize'] = None
+            self.orbitdb_args['swarmName'] = None
+
+        else:
+            if 'ipaddress' in self.meta:
+                self.orbitdb_args['ipaddress'] = self.meta['ipaddress']
+            else:
+                self.orbitdb_args['ipaddress'] = None
+            
+            if 'orbitdbAddress' in self.meta:
+                self.orbitdb_args['orbitdbAddress'] = self.meta['orbitdbAddress']
+            else:
+                self.orbitdb_args['orbitdbAddress'] = None
+            
+            if 'index' in self.meta:
+                self.orbitdb_args['index'] = self.meta['index']
+            else:
+                self.orbitdb_args['index'] = None
+            
+            if 'chunkSize' in self.meta:
+                self.orbitdb_args['chunkSize'] = self.meta['chunkSize']
+            else:
+                self.orbitdb_args['chunkSize'] = None
+
+            if 'swarmName' in self.meta:
+                self.orbitdb_args['swarmName'] = self.meta['swarmName']
+            else:
+                self.orbitdb_args['swarmName'] = None
+        
+        if self.orbitdb_args['ipaddress'] is None:
+            self.orbitdb_args['ipaddress'] = '127.0.0.1'
+        if self.orbitdb_args['orbitdbAddress'] is None:
+            self.orbitdb_args['orbitdbAddress'] = None
+        if self.orbitdb_args['index'] is None:
+            self.orbitdb_args['index'] = 1
+        if self.orbitdb_args['chunkSize'] is None:
+            self.orbitdb_args['chunkSize'] = 8
+        if self.orbitdb_args['swarmName'] is None:
+            self.orbitdb_args['swarmName'] = "caselaw"
+        if self.orbitdb_args['port'] is None:
+            self.orbitdb_args['port'] = 50001
+        pass
+
+    def start_orbitdb(self):
+        start_orbitdb = process.Popen(['orbitdb', 'start'], stdout=process.PIPE, stderr=process.PIPE)
+        pass
+
+    def stop_orbitdb(self):
+        stop_orbitdb = process.Popen(['orbitdb', 'stop'], stdout=process.PIPE, stderr=process.PIPE)
+        pass
+
+    def get_resources(self):
+        return self.resources
+    
+if __name__ == '__main__':
+    resources = {}
+    meta = {}
+    orbit_kit = orbit_kit(resources, meta)
+    print(orbit_kit.get_resources())
+
